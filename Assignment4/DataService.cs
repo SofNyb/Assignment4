7c994eaac2cc76198d75cff287f4708af9bd368a
SofNyb <snjwindeloev@gmail.com>
2025-10-14 14:19:30 +0200

    Cleanup on DataService

--- Assignment4/DataService.cs
+++ Assignment4/DataService.cs
@@ -6,11 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Assignment4;
+using ProductWithCategory = Assignment4.ProductOperations.ProductWithCategory;
+using ProductWithName = Assignment4.ProductOperations.ProductWithName;
+
 
 namespace Assignment4
 {
     public class DataService
     {
+        /* Category */
         private readonly CategoryOperations _categoryOperations = new CategoryOperations();
 
         public List<Category> GetCategories()
@@ -38,55 +42,22 @@ namespace Assignment4
             return _categoryOperations.UpdateCategory(id, name, description);
         }
 
+        /* Products */
+        private readonly ProductOperations _productOperations = new ProductOperations();
+
         public Product GetProduct(int id)
         {
-            using var db = new NorthwindContext();
-            return db.Products.Include(p => p.Category).FirstOrDefault(p => p.Id == id);
-        }
-
-        public class ProductWithCategory
-        {
-            public string Name { get; set; }
-            public string CategoryName { get; set; }
+            return _productOperations.GetProduct(id);
         }
 
         public List<ProductWithCategory> GetProductByCategory(int id)
         {
-            using var db = new NorthwindContext();
-            var products = db.Products
-                .Include(p => p.Category)
-                .Where(p => p.CategoryId == id)
-                .AsNoTracking()
-                .ToList();
-
-            return products
-                .Select(p => new ProductWithCategory
-                {
-                    Name = p.Name,
-                    CategoryName = p.Category?.Name
-                })
-                .ToList();
-        }
-
-        public class ProductWithName
-        {
-            public string ProductName { get; set; }  
+            return _productOperations.GetProductByCategory(id);
         }
 
         public List<ProductWithName> GetProductByName(string name)
         {
-            using var db = new NorthwindContext();
-            var products = db.Products
-                .Where(p => EF.Functions.ILike(p.Name, $"%{name}%"))
-                .AsNoTracking()
-                .ToList();
-
-            return products
-                .Select(p => new ProductWithName
-                {
-                    ProductName = p.Name
-                })
-                .ToList();
+            return _productOperations.GetProductByName(name);
         }
 
     }
---
+++ Assignment4/ProductOperations.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class ProductOperations
+    {
+        public class ProductWithCategory
+        {
+            public string Name { get; set; }
+            public string CategoryName { get; set; }
+        }
+
+        public class ProductWithName
+        {
+            public string ProductName { get; set; }
+        }
+
+        public Product GetProduct(int id)
+        {
+            using var db = new NorthwindContext();
+            return db.Products.Include(p => p.Category).FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<ProductWithCategory> GetProductByCategory(int id)
+        {
+            using var db = new NorthwindContext();
+            var products = db.Products
+                .Include(p => p.Category)
+                .Where(p => p.CategoryId == id)
+                .AsNoTracking()
+                .ToList();
+
+            return products
+                .Select(p => new ProductWithCategory
+                {
+                    Name = p.Name,
+                    CategoryName = p.Category?.Name
+                })
+                .ToList();
+        }
+
+        public List<ProductWithName> GetProductByName(string name)
+        {
+            using var db = new NorthwindContext();
+            var products = db.Products
+                .Where(p => EF.Functions.ILike(p.Name, $"%{name}%"))
+                .AsNoTracking()
+                .ToList();
+
+            return products
+                .Select(p => new ProductWithName
+                {
+                    ProductName = p.Name
+                })
+                .ToList();
+        }
+
+    }
+}
