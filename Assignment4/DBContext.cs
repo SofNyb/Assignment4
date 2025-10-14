77c3b6ac9d44739636f12a9ebcaa1b00eea48ab4
SofNyb <snjwindeloev@gmail.com>
2025-10-14 14:38:54 +0200

    updated context

--- Assignment4/DBContext.cs
+++ Assignment4/DBContext.cs
@@ -8,6 +8,8 @@ public class NorthwindContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Category> Category { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -27,7 +29,22 @@ public class NorthwindContext : DbContext
         modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
         modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
         modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
-        
+
+        modelBuilder.Entity<Order>().ToTable("orders");
+        modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+        modelBuilder.Entity<Order>().Property(x => x.CustomerId).HasColumnName("customerid");
+        modelBuilder.Entity<Order>().Property(x => x.OrderDate).HasColumnName("orderdate");
+        modelBuilder.Entity<Order>().Property(x => x.RequiredDate).HasColumnName("requireddate");
+        modelBuilder.Entity<Order>().Property(x => x.ShippedDate).HasColumnName("shippeddate");
+        modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname");
+        modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
+
+        modelBuilder.Entity<OrderDetails>().ToTable("orderdetails");
+        modelBuilder.Entity<OrderDetails>().Property(x => x.OrderId).HasColumnName("orderid");
+        modelBuilder.Entity<OrderDetails>().Property(x => x.ProductId).HasColumnName("productid");
+        modelBuilder.Entity<OrderDetails>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+        modelBuilder.Entity<OrderDetails>().Property(x => x.Quantity).HasColumnName("quantity");
+        modelBuilder.Entity<OrderDetails>().Property(x => x.Discount).HasColumnName("discount");
 
     }
 }
