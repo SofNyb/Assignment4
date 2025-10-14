a1f3d08e85494e404f24f5cda0e9721fb7f214c0
SofNyb <snjwindeloev@gmail.com>
2025-10-14 15:42:27 +0200

    added Orders to orderdetails

--- Assignment4/OrderDetail.cs
+++ Assignment4/OrderDetail.cs
@@ -14,5 +14,6 @@ namespace Assignment4
         public int Quantity { get; set; }
         public int Discount { get; set; }
         public Product Product { get; set; }
+        public Order Order { get; set; }
     }
 }
