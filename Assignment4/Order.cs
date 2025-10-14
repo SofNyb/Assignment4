419128ef7ce98313008b830c306a8b1df97f51d3
SofNyb <snjwindeloev@gmail.com>
2025-10-14 14:39:15 +0200

    cleanup

---
+++ Assignment4/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+    }
+}
