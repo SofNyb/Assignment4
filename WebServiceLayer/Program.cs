801e3cc70f405de1e1c557f4a17123cd7c05d344
SofNyb <snjwindeloev@gmail.com>
2025-10-16 13:28:47 +0200

    Added webservicelayer files

---
+++ WebServiceLayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using DataServiceLayer;
+
+namespace WebServiceLayer;
+
+public class Program
+{
+    public static DataService DataService { get; } = new DataService();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<DataService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
